05539b6a70bd087e61acb4f05dfbfb68a5eb13cc
raffiner12 <118718433+raffiner12@users.noreply.github.com>
2023-02-13 15:03:55 +0900

    Update WebConfig.java

--- spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
+++ spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**") // 매핑이름
-		.addResourceLocations(""file:///usr/local/project/product_images/");
+		.addResourceLocations("file:///usr/local/project/product_images/");
 				      //file:////Users/pizza/STS3/SpringWorkspace/product_images/",
 							 // "file:////Users/pizza/STS3/SpringWorkspace/upload/");
 								// upload 폴더 경로 설정(동일한 이름 /images 사용)
